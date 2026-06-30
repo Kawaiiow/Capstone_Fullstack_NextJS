425ad95ca69a0969261a8d9ae6ea7c9207e5a3b7
Kawaiiow <poohsn1357@gmail.com>
2026-06-30 22:56:25 +0700

    build: add react compiler and edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  // Development Configuration
+  logging: {
+    browserToTerminal: "error",
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
