66e6d28ec67a6bc853806fac58d4bc682517cc2b
SudJarb007 <PrapassornNou@gmail.com>
2026-07-04 16:32:59 +0700

    fix: correct supabase anon key env variable name

--- libs/supabase.js
+++ libs/supabase.js
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 // Browser client (singleton)
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createBrowserClient(supabaseUrl, supabaseKey)
 
 export default supabase
